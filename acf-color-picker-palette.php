a5e37e50195f7f0907c9922f51eca2bbb96e1cb9
Timothy Jensen <tim@timjensen.us>
2018-01-03 16:36:19 -0500

    MIT license

--- acf-color-picker-palette.php
+++ acf-color-picker-palette.php
@@ -6,7 +6,7 @@
  * Version: 1.0.0
  * Author: Tomodomo
  * Author URI: https://tomodomo.co/
- * License: GPLv2 or later
+ * License: MIT
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
